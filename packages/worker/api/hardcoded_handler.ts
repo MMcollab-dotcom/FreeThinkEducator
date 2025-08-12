22acf5468084ba0357e6e909118de1f52e7d10ac
Mengtao Zhang <mengtao.zhang@inf.ethz.ch>
2025-08-12 23:30:10 +0200

    more hardcode

--- packages/worker/api/hardcoded_handler.ts
+++ packages/worker/api/hardcoded_handler.ts
@@ -1,17 +1,26 @@
 // Predefined texts to stream
 const predefinedTexts = [
-    ["User Profile:",
-    "\n-Profession: Biologist",
+    ["User profile:",
     "\n-Skills: Biology, Statistics",
+    "\n-Weakness: DL models",
     "\n-Focus: Deep learning for time-series medical data",
-    "\nDo you wanna know more about time series deep learning models?"],
+    "\n\nBased on description, time series model could be suitable.",
+    "\n\nDo you want to know more about them?"],
     [ "- LSTM\n- RNN\n- Transformer" ],
-    [ "LSTM is a blablabla" ],
-    [ "- Hello" ],
-    [ "RNN is a blablabla"],
+    [ " ⁠RNN: recurrent neural networks, Processes sequences step-by-step, passing hidden state from one timestep to the next. Lower compute cost than LSTM or Transformers but struggles with very long range dependencies" ],
     [""],
-    [ "LSTM is a mathematica"],
-    [""]
+    [ "LSTM: An RNN variant with gates to control what information is kept, forgotten, or output at each step. Handles longer sequences than default RNNs, but becomes very slow with long sequences."],
+    [""],
+    [ "Transformers: State of the art in time series modelling, efficiently trains long term dependencies in parallel with self attention. Best at modelling long range dependences and fast to train, but needs more data and high memory usage" ],
+    [ "- How much data is there?\n- How much compute is available?\n- What coding skills does user have" ],
+    ["User profile:",
+    "\n-Updated Skills: Python, Biology, Statistics",
+    "\n-Target: DL for medical time series",
+    "\n\nSounds like LSTMs are a good tradeoff between modelling capabilties and compute availability.    ",
+    "\n\nSince you have python skills, here is Pytorch LSTM docs and tutorials:",
+    "\nhttps://docs.pytorch.org/docs/stable/generated/torch.nn.LSTM.html"],
+    [""],
+
 ];
 
 export const hardcoded_handler = async (req: Request): Promise<Response> => {
@@ -23,7 +32,7 @@ export const hardcoded_handler = async (req: Request): Promise<Response> => {
     const encoder = new TextEncoder();
     let index = 0;
     const global_index = parseInt(prompt.split(' ')[0], 10);
-
+    console.log( prompt )
     const stream = new ReadableStream({
       async start(controller) {
         let answer = predefinedTexts[global_index]
